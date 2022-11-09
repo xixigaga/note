d77db71d99ed5beb12ac16eb1f37a58642ced828
Jared <xixigaga@sina.com>
2022-11-09 11:58:18 +0800

    Delete script directory

--- script/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /usr/local/alipay/service
->nohup.out
-nohup java -Xms10m -Xmx500m -jar alipay-life-service.jar --spring.profiles.active=pub &
-proc_name="alipay-service.jar"
-name_suffix="\>"
-proc_id=`ps -ef|grep -i ${proc_name}${name_suffix}|grep -v "grep"|awk '{print $2}'`
-echo ${proc_name}" pid:"echo ${proc_id[@]}
